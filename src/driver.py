af22927317590ea9c00996b8562a71c0a50ea515
dbarry9 <dbarry@vols.utk.edu>
2018-10-24 14:49:13 -0400

    Added empty template for data structure attributes and some basic features to the driver program.

---
+++ src/driver.py
@@ -0,0 +1,21 @@
+########################################
+# Project: Video File Uniqueness
+# Professor: Dr. Audris Mockus
+########################################
+
+import argparse
+import pandas as pd
+import glob
+
+## Parse the arguments.
+parse = argparse.ArgumentParser()
+parse.add_argument("data_path", help="Absolute path of directory containing the raw data.")
+parsedargs = parse.parse_args()
+
+## Check for errors in the command-line arguments.
+path = parsedargs.data_path
+
+## Read-in the data structure template.
+template = pd.read_csv("template.csv")
+
+## Use glob to generate list of all video files in the specified path.
