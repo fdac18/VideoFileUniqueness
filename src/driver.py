94a61b0018ef5066c61c8b5d24815f44f7a8c9b8
dbarry9 <dbarry@vols.utk.edu>
2018-11-07 14:45:42 -0500

    Added programming task for Cole F.

--- src/driver.py
+++ src/driver.py
@@ -15,7 +15,11 @@ parsedargs = parse.parse_args()
 ## Check for errors in the command-line arguments.
 path = parsedargs.data_path
 
+## Generate list of strings of video file names using glob or subprocess. #ColeF
+
 ## Read-in the data structure template.
 template = pd.read_csv("template.csv")
+
+
 
 ## Use glob to generate list of all video files in the specified path.
