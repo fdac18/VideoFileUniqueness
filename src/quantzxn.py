22712b650beab0325ef6de1cbddc72251a603ca5
hossain-rayhan <hossain.rayhan@outlook.com>
2018-11-09 13:55:23 -0500

    #9 quantize_image() function returns the array of rgb values for k-colors

--- src/quantzxn.py
+++ src/quantzxn.py
@@ -4,25 +4,31 @@
 import numpy as np
 import cv2 
 
-img = cv2.imread('home.png')
-Z = img.reshape((-1,3))
 
-# convert to np.float32
-Z = np.float32(Z)
+def quantize_image(file_path, K = 5):
 
-# define criteria, number of clusters(K) and apply kmeans()
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 5
-ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-print(ret)
+	img = cv2.imread(file_path)
+	Z = img.reshape((-1,3))
 
-# Now convert back into uint8, and make original image
-center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape((img.shape))
+	# convert to np.float32
+	Z = np.float32(Z)
 
-cv2.imshow('res2',res2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	# define criteria, number of clusters(K) and apply kmeans()
+	criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+	ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+	# Now convert back into uint8, and make original image
+	center = np.uint8(center)
+	#res = center[label.flatten()]
+	#res2 = res.reshape((img.shape))
+
+	#cv2.imshow('res2',res2)
+	#cv2.waitKey(0)
+	#cv2.destroyAllWindows()
+
+	return center
 
 
+rgbs = quantize_image('home.png', 5)
+print("RGB Values", rgbs)
